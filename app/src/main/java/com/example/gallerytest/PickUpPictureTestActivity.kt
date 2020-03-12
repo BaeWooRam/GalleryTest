508beaf2068df1fce5a842fd0a76d7a39b4b0a38
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2020-03-12 18:59:48 +0900

    03-12

--- app/src/main/java/com/example/gallerytest/PickUpPictureTestActivity.kt
+++ app/src/main/java/com/example/gallerytest/PickUpPictureTestActivity.kt
@@ -1,19 +1,38 @@
 package com.example.gallerytest
 
-import android.content.ContentUris
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.example.gallerytest.base.PermissionActivity
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class PickUpPictureTestActivity : PermissionActivity(R.layout.activity_main) {
+    private var _CurrentPhotoPath:String = ""
+    private var _imgUri: Uri? = null
+    private val FROM_CAMERA = 0
+
     override fun getCheckPermission(): Array<String> {
         return arrayOf("android.permission.CAMERA", "android.permission.READ_EXTERNAL_STORAGE")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(permission.isGrantPermission)
+            takePhoto()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -21,7 +40,7 @@ class PickUpPictureTestActivity : PermissionActivity(R.layout.activity_main) {
 
         if(permission.isRequestCode(requestCode) && permission.isGrantResults(grantResults)) {
             GlobalScope.async {
-
+                takePhoto()
             }
         }else {
             finish()
@@ -29,5 +48,79 @@ class PickUpPictureTestActivity : PermissionActivity(R.layout.activity_main) {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == FROM_CAMERA && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            Log.d("FROM_CAMERA", imageBitmap.toString())
+        }
+    }
+
+    /**
+     * 카메라 찍는 Intent 보낸다.
+     */
+    private fun takePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            .also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, FROM_CAMERA)
+            }
+        }
+    }
+
+    /**
+     * 파일 생성하고 Path 저장.
+     */
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            _CurrentPhotoPath = absolutePath
+        }
+    }
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Log.e("createImageFile", "Erro Create File")
+                    null
+                }
+
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(_CurrentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
 
 }
